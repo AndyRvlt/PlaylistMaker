c6ebf1505a23eae1c47a47b968615a588f69c0a0
revol <AndyRevolt@yandex.ru>
2023-07-28 19:43:56 +0300

    PlaylistMarket/Домашняя работа

---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,19 @@
+package com.example.playlistmaker
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+       val buttonArrowBack = findViewById<Button>(R.id.arrowBack)
+        buttonArrowBack.setOnClickListener{
+            val displayMain = Intent(this, MainActivity::class.java)
+            startActivity(displayMain)
+        }
+    }
+}

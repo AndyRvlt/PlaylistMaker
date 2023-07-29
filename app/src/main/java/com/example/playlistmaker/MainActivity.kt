b59158f174a1dd077e1f57535cf09eb77b166dde
AndyRvlt <124528093+AndyRvlt@users.noreply.github.com>
2023-07-29 21:39:02 +0300

    Revert "PlaylistMarket"

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,39 +1,11 @@
 package com.example.playlistmaker
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val buttonSearch = findViewById<Button>(R.id.buttonSearch)
-        val buttonMedia = findViewById<Button>(R.id.buttonMedia)
-        val buttonSettings = findViewById<Button>(R.id.buttonSettings)
-
-
-
-
-        buttonMedia.setOnClickListener {
-            val displayMedia = Intent(this, MediaActivity::class.java)
-            startActivity(displayMedia)
-
-        }
-        val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                val displaySearch = Intent(this@MainActivity, SearchActivity::class.java)
-                startActivity(displaySearch)
-            }
-        }
-        buttonSearch.setOnClickListener(buttonSearchClickListener)
-
-        buttonSettings.setOnClickListener {
-            val displaySettings = Intent(this, SettingsActivity::class.java)
-            startActivity(displaySettings)
-        }
     }
 }
